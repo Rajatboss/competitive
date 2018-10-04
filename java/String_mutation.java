a263421c4ae87c95761782b9f3c6ec063f977049
Rajatboss <32414270+Rajatboss@users.noreply.github.com>
2018-10-04 17:31:03 +0530

    StringMutation

---
+++ java/String_mutation.java
@@ -0,0 +1,13 @@
+
+public class String_mutation {
+
+	public static void main(String[] args) {
+			System.out.println("Rajat".length());
+			System.out.println("Rajat".concat(" Piyush"));
+			System.out.println("Rajat".equals("Rajat"));
+			System.out.println("Rajat".substring(0, 3));
+			System.out.println("     rajat     ".trim());
+
+	}
+
+}
